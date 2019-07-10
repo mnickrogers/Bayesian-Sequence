b2d347299bc8b872a76edad76f9e5611aee2da46
Nick Rogers <mnicholasrogers@gmail.com>
2019-07-10 16:57:21 -0700

    Added files for Observation class.

---
+++ Bayesian-Sequence/Bayesian-Sequence/Observation.cpp
@@ -0,0 +1,9 @@
+//
+//  Observation.cpp
+//  Bayesian-Sequence
+//
+//  Created by Nicholas Rogers on 7/10/19.
+//  Copyright © 2019 Nicholas Rogers. All rights reserved.
+//
+
+#include "Observation.hpp"
---
+++ Bayesian-Sequence/Bayesian-Sequence/Observation.hpp
@@ -0,0 +1,14 @@
+//
+//  Observation.hpp
+//  Bayesian-Sequence
+//
+//  Created by Nicholas Rogers on 7/10/19.
+//  Copyright © 2019 Nicholas Rogers. All rights reserved.
+//
+
+#ifndef Observation_hpp
+#define Observation_hpp
+
+#include <stdio.h>
+
+#endif /* Observation_hpp */
