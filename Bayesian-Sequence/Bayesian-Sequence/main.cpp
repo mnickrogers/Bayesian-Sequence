9522055c9b37b47fbdc87bd7a4fff371520e8adc
Nick Rogers <mnicholasrogers@gmail.com>
2019-07-10 16:42:44 -0700

    Initial commit.

---
+++ Bayesian-Sequence/Bayesian-Sequence/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bayesian-Sequence
+//
+//  Created by Nicholas Rogers on 7/10/19.
+//  Copyright © 2019 Nicholas Rogers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
