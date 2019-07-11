8abe5ddaaeeadf05c211d2a586970217f9817f5f
Nick Rogers <mnicholasrogers@gmail.com>
2019-07-10 17:04:42 -0700

    Added files for event class.

---
+++ Bayesian-Sequence/Bayesian-Sequence/Event.cpp
@@ -0,0 +1,9 @@
+//
+//  Event.cpp
+//  Bayesian-Sequence
+//
+//  Created by Nicholas Rogers on 7/10/19.
+//  Copyright © 2019 Nicholas Rogers. All rights reserved.
+//
+
+#include "Event.hpp"
---
+++ Bayesian-Sequence/Bayesian-Sequence/Event.hpp
@@ -0,0 +1,14 @@
+//
+//  Event.hpp
+//  Bayesian-Sequence
+//
+//  Created by Nicholas Rogers on 7/10/19.
+//  Copyright © 2019 Nicholas Rogers. All rights reserved.
+//
+
+#ifndef Event_hpp
+#define Event_hpp
+
+#include <stdio.h>
+
+#endif /* Event_hpp */
