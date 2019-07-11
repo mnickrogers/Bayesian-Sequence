5301f373c6bf9017b7c76191d1647af80e564385
Nick Rogers <mnicholasrogers@gmail.com>
2019-07-10 17:04:07 -0700

    Added description above observation class.

--- Bayesian-Sequence/Bayesian-Sequence/Observation.hpp
+++ Bayesian-Sequence/Bayesian-Sequence/Observation.hpp
@@ -11,4 +11,19 @@
 
 #include <stdio.h>
 
+/*
+ The observation class handles a single observation of data.
+ 
+ It contains an association of an event with a prediction.
+ For example, let's say you were predicting the probability the stock market
+ rising tomorrow morning. A successful jobs report would be a good
+ indicator. The release of the jobs report is the event and the association
+ between the jobs report and the prediction (the stock market rising) would
+ be the observation.
+ 
+ */
+class Observation
+{
+};
+
 #endif /* Observation_hpp */
